2b3754f6f62275fdaeb1bce62943f2b44c9db010
Justin Halsall <Juice10@users.noreply.github.com>
2015-11-01 02:14:52 +0100

    tweak gauge

--- gauge.js
+++ gauge.js
@@ -17,11 +17,13 @@ setTimeout(function () {
          label: {
              format: function(value, ratio) {
                if (value < 30) {
+                 return 'Beer time!';
+               } else if (value < 70) {
+                 return 'Perfect!';
+               } else if (value < 90) {
                  return '200% fun!';
-               } else if (value < 80) {
-                 return 'perfect!';
                } else {
-                 return 'beer time!';
+                 return 'You drunk!';
                }
              },
              show: false // to turn off the min/max labels.
@@ -32,11 +34,11 @@ setTimeout(function () {
   //    width: 39 // for adjusting arc thickness
     },
     color: {
-        pattern: ['#F6C600', '#60B044', '#4466FF'], // the three color levels for the percentage values.
+        pattern: ['#4466FF', '#60B044', '#F6C600', '#FF0000'], // the three color levels for the percentage values.
         threshold: {
   //            unit: 'value', // percentage is default
   //            max: 200, // 100 is default
-            values: [30, 80, 100]
+            values: [30, 70, 90, 100]
         }
     },
     tooltip: {
@@ -88,8 +90,8 @@ setTimeout(function () {
   };
 
   Gauge.update = function () {
-    var cardios = (this._cardio + caloriesPerBeer) / caloriesPerBeer;
-    var ratio = (cardios || 0.0001) / (this._beer + 1 || 0.0001);
+    var cardios = (this._cardio + (caloriesPerBeer * 2)) / caloriesPerBeer;
+    var ratio = (this._beer + 2 || 0.0001) / (cardios || 0.0001);
 
     chart.load({
         columns: [['data', ratio * 50]]
